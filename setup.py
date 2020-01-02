1731a8f774bf53c135033c29f463a4b37b55c3cf
Arun Tejasvi Chaganty <arun@squareup.com>
2020-01-01 22:37:20 -0800

    Added a setup.py so that this package can be installed

---
+++ setup.py
@@ -0,0 +1,18 @@
+import re
+import sysconfig
+from subprocess import check_output
+
+
+from setuptools import setup, Extension, find_packages
+
+setup(
+    name='embedding_evaluation',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='justin',
+    author_email='jdieter@stanford.edu',
+    description='',
+    install_requires=[]
+)
